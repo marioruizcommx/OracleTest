ec4bc239ffd77c19987455c3260811af87bb3541
root <root@localhost.localdomain>
2020-04-08 19:38:54 -0500

    nueva lista

--- src/main/java/com/gym/Certifications/controller/RoboticsController.java
+++ src/main/java/com/gym/Certifications/controller/RoboticsController.java
@@ -1,8 +1,13 @@
 package com.gym.Certifications.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class RoboticsController {
@@ -13,6 +18,26 @@ public class RoboticsController {
 			ModelAndView mav = new ModelAndView();
 			return mav;
 			
+		}
+		
+		@GetMapping("/validate-key")
+		public String updateJuridiccion(@RequestParam(name = "key", required= false) Integer key ,Model model) {
+			//
+			System.out.println("--------------------------"+key);
+			
+			if (key == null) {
+				key=1427538;
+			}
+			
+			if(key==142753869) {
+				//return "redirect:/robotics";
+				return "redirect:/assets/docs/arduinok545_tinkercad.brd";
+				
+			}else {
+				return "redirect:/assets/docs/arduinok545_tinkercad.brd";
+				
+			}
+
 		}
 
 	}
